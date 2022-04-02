3071a334d69c45099ac76c01e1a7422ad22a8b42
arcanfe@nospam <arcanfe@nospam>
2022-04-02 09:01:39 +0000

    split out scanning helpers into default
    FossilOrigin-Name: f0267216531d99e3455e6f486981d57b239dc846c6d576b7797646d8d6f5ca9f

--- cat9.lua
+++ cat9.lua
@@ -138,7 +138,17 @@ local function load_builtins(base)
 	for k, _ in pairs(builtins) do
 		table.insert(builtin_completion, k)
 	end
+
 	table.sort(builtin_completion)
+end
+
+local function load_feature(name)
+	fptr, msg = loadfile(lash.scriptdir .. "./cat9/base/" .. name)
+	if not fptr then
+		return false, msg
+	end
+	local init = fptr()
+	init(cat9, root)
 end
 
 function cat9.run_lut(cmd, tgt, lut, set)
@@ -173,6 +183,7 @@ end
 
 function handlers.recolor()
 	cat9.redraw()
+	cat9.flag_dirty()
 end
 
 function handlers.visibility(self, visible, focus)
@@ -1414,169 +1425,6 @@ function cat9.flag_dirty()
 	cat9.dirty = true
 end
 
-function cat9.prefix_filter(intbl, prefix, offset)
-	local res = {}
-
-	for _,v in ipairs(intbl) do
-		if string.sub(v, 1, #prefix) == prefix then
-			local str = v
-			if offset then
-				str = string.sub(v, offset)
-			end
-			if #str > 0 then
-				table.insert(res, str)
-			end
-		end
-	end
-
--- special case, we already have what we suggest, set to empty so the readline
--- implementation can autocommit on linefeed
-	if #res == 1 then
-		local sub = offset and string.sub(prefix, offset) or prefix
-		if sub and sub == res[1] then
-			return {}
-		end
-	end
-
-	table.sort(res)
-	return res
-end
-
--- This can be called either when invalidating an ongoing scanner by setting a
--- new, or cancelling ongoing scanning due to the results not being interesting
--- anymore. It does not actually stop immediately, but rather kill the related
--- process (if still alive) so the normal job management will flush it out.
-function cat9.stop_scanner()
-	if not cat9.scanner.active then
-		return
-	end
-
-	if cat9.scanner.pid then
-		root:psignal(cat9.scanner.pid, "kill")
-		cat9.scanner.pid = nil
-	end
-
--- not to be confused with the job closure table
-	if cat9.scanner.closure then
-		cat9.scanner.closure()
-	end
-
-	cat9.scanner.closure = nil
-	cat9.scanner.active = nil
-end
-
---
--- run [path (str | argtbl) and trigger closure with the dataset when completed.
--- should only be used for singleton short-lived, line-separated fast commands
--- used for tab completion
---
-function cat9.set_scanner(path, closure)
-	cat9.stop_scanner()
-
-	local _, out, _, pid = root:popen(path, "r")
-
-	if not pid then
-		if config.debug then
-			print("failed to spawn scanner job:", path)
-		end
-		return
-	end
-
--- the pid will be wait():ed / killed as part of job control
-	cat9.scanner.pid = pid
-	cat9.scanner.closure = closure
-	cat9.scanner.active = path
-
--- mark as hidden so it doesn't clutter the UI or consume job IDs but can still
--- re-use event triggers an asynch processing
-	local job =
-	{
-		out = out,
-		pid = pid,
-		hidden = true,
-	}
-
-	cat9.import_job(job)
-	out:lf_strip(true)
-
--- append as closure so it'll be triggers just like any other job completion
-	table.insert(job.closure,
-	function(id, code)
-		cat9.scanner.pid = nil
-		if cat9.scanner.closure then
-			cat9.scanner.closure(job.data)
-		end
-	end)
-
-end
-
--- calculate the suggestion- set parameters to account for absolute/relative/...
-function cat9.file_completion(fn)
-	local path   -- actual path to search
-	local prefix -- prefix to filter from last path when applying completion
-	local flt    -- prefix to filter away from result-set
-	local offset -- add item to suggestion starting at offset after prefix match
-
--- args are #1 (cd) or #2 (cd <path>)
-	if not fn or #fn == 0 then
-		path = "./"
-		prefix = ""
-		flt = "./"
-		offset = 3
-		return path, prefix, flt, offset
-	end
-
--- $env expansion not considered, neither is ~ at the moment
-	local elements = string.split(fn, "/")
-	local nelem = #elements
-
-	path = table.concat(elements, "/", 1, nelem - 1)
-	local ch = string.sub(fn, 1, 1)
-
--- explicit absolute
-	if ch == '/' then
-		offset = #path + 2
-		if #elements == 2 then
-			path = "/" .. path
-		end
-		prefix = path .. (#path > 1 and "/" or "")
-
-		if nelem == 2 then
-			flt = path .. elements[nelem]
-		else
-			flt = path .. "/" .. elements[nelem]
-		end
-		return path, prefix, flt, offset
-	end
-
--- explicit relative
-	if string.sub(fn, 1, 2) == "./" then
-		offset = #path + 2
-		prefix = path .. "/"
-		flt = path .. "/" .. elements[nelem]
-		return path, prefix, flt, offset
-	end
-
-	if string.sub(fn, 1, 3) == "../" then
-		offset = #path + 2
-		prefix = path .. "/"
-		flt = path .. "/" .. elements[nelem]
-		return path, prefix, flt, offset
-	end
-
-	prefix = path
-	path = "./" .. path
-	if nelem == 1 then
-		flt = path .. elements[nelem]
-		offset = #path + 1
-	else
-		flt = path .. "/" .. elements[nelem]
-		prefix = prefix .. "/"
-		offset = #path + 2
-	end
-	return path, prefix, flt, offset
-end
-
 function cat9.setup_shell_job(args, mode, envv)
 -- could pick some other 'input' here, e.g.
 -- .in:stdin .env:key1=env;key2=env mycmd $2 arg ..
@@ -1678,6 +1526,7 @@ function cat9.parse_string(rl, line)
 	end
 end
 
+load_feature("scanner.lua")
 -- use mouse-forward mode, implement our own selection / picking
 load_builtins("default.lua")
 config.readline.verify = cat9.readline_verify
