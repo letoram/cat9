1013beaeb1d0c06a51e7c0dc484659f1c02c8777
void <>
2024-08-20 12:49:38 +0000

    add config for spreadsheet
    FossilOrigin-Name: 53c48d532c533aa256fc7e7fabbba4637740f8a00e388bfc3e890120d94bfe07

---
+++ cat9/config/spreadsheet.lua
@@ -0,0 +1,23 @@
+return {
+	min_col_width = 5,
+	label_1 = {
+		fc = tui.colors.label, bc = tui.colors.text
+	},
+	default_border = {
+		border_top = true,
+		border_left = true,
+		border_right = true,
+		border_bottom = true
+	},
+	label_2 = {
+
+	},
+	col_1 = {
+		fc = tui.colors.ref_light_grey,
+		bc = tui.colors.ref_light_grey
+	},
+	col_2 = {
+		fc = tui.colors.ref_grey,
+		bc = tui.colors.ref_grey
+	}
+}
